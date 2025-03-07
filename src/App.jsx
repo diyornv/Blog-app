5b3a06401c0ea54dc8122c7520ea32d960c53e9f
diyornv <diyorcode@gmail.com>
2025-03-07 19:19:57 +0500

    bugg

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import './index.css'
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-    return (
-        <div className='container mx-auto'>
-            <h1 className='text-4xl'>hello</h1>
-            <h1 className='text-6xl'>hello</h1>
-        </div>
-    )
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl">hello</h1>
+      <h1 className="text-6xl">hello</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    theme: {
-      extend: {
-        container: {
-          center: true, // Har doim o‘rtaga joylash
-          screens: {
-            xl: "1440px", // Katta ekranlar (Desktop)
-            lg: "834px", // Planshetlar
-            sm: "390px", // Telefonlar
-          },
-          padding: {
-            DEFAULT: "112px", // Odatiy holatda 112px (katta ekranlar)
-            lg: "20px", // Planshetlarda 20px padding
-            sm: "32px", // Telefonlarda 32px padding
-          },
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true, // Har doim o‘rtaga joylash
+        screens: {
+          xl: "1440px", // Katta ekranlar (Desktop)
+          lg: "834px", // Planshetlar
+          sm: "390px", // Telefonlar
+        },
+        padding: {
+          DEFAULT: "112px", // Odatiy holatda 112px (katta ekranlar)
+          lg: "20px", // Planshetlarda 20px padding
+          sm: "32px", // Telefonlarda 32px padding
         },
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
